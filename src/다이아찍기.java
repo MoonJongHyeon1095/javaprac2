0096addacf81f4935e3436124a9bb0e976c03e45
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-07-12 16:50:13 +0900

    printDiamond

--- src/다이아찍기.java
+++ src/다이아찍기.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class 다이아찍기 {
+public class 다이아찍기 {
+    public static void main(String[] args) {
+        printDiamond(5);
+
+    }
+
+    private static void printDiamond(int size){
+        int maxLength = size*2 -1;
+        int 중간지점 = maxLength/2;
+
+        for (int i = 0; i < maxLength; i++) {
+            for (int j = 0; j < Math.abs(중간지점 - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < maxLength - Math.abs(중간지점 - i) * 2; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+    }
 }
