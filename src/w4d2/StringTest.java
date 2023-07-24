c14d900d8004b94adfdfa7be9b52897c118b43fb
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-07-24 13:51:20 +0900

    w4d2

--- src/w4d2/StringTest.java
+++ src/w4d2/StringTest.java
@@ -12,6 +12,7 @@ public class StringTest {
         System.out.println("str1과 str2의 참조 비교 결과: " + (str1 == str2 ? "같은 참조" : "다른 참조"));
         System.out.println("str1과 str3의 참조 비교 결과: " + (str1 == str3 ? "같은 참조" : "다른 참조"));
 
+        //== 연산자는 두 객체의 참조를 비교하고, equals() 메서드는 두 객체의 값을 비교
         //TODO: str1 <-> str2, str1 <-> str3 를 equals 으로 값을 비교 및 출력해주세요 .
         System.out.println("str1과 str2의 비교 결과: " + (str1.equals(str2) ? "같음" : "다름"));
         System.out.println("str1과 str3의 비교 결과: " + (str1.equals(str3) ? "같음" : "다름"));
